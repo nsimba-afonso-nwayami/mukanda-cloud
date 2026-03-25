acf1df0a4b077f84316f232829c1818d1c7d4688
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-03-25 16:47:55 +0100

    Página notfound do super admin refeito

--- src/pages/superadmin/NotFoundSuperAdmin.jsx
+++ src/pages/superadmin/NotFoundSuperAdmin.jsx
@@ -4,31 +4,31 @@ import SuperAdminLayout from "./components/SuperAdminLayout";
 export default function NotFoundSuperAdmin() {
   return (
     <>
-      <title>Página não encontrada | Dashboard Hospital</title>
+      <title>Página não encontrada | SuperAdmin</title>
 
       <SuperAdminLayout title="Página não encontrada">
-        <main className="flex flex-col items-center justify-center min-h-[70vh] bg-slate-50 px-6 text-center py-24">
+        <main className="flex flex-col items-center justify-center min-h-[70vh] bg-slate-950 px-6 text-center py-24">
           {/* Ícone animado */}
-          <i className="fa-solid fa-hospital text-teal-500 text-5xl mb-4 fa-beat"></i>
+          <i className="fa-solid fa-cloud text-cyan-500 text-5xl mb-4 fa-beat"></i>
 
           {/* Número 404 */}
-          <h1 className="text-6xl font-extrabold text-teal-500 mb-4">404</h1>
+          <h1 className="text-6xl font-extrabold text-cyan-500 mb-4">404</h1>
 
           {/* Título */}
-          <h2 className="text-xl md:text-2xl font-bold text-slate-800 mb-2">
+          <h2 className="text-xl md:text-2xl font-bold text-slate-100 mb-2">
             Oops! Página não encontrada
           </h2>
 
           {/* Descrição */}
-          <p className="text-slate-500 mb-6 max-w-md text-sm md:text-base">
+          <p className="text-slate-400 mb-6 max-w-md text-sm md:text-base">
             A página que você está tentando acessar não existe, foi removida ou
             está temporariamente indisponível.
           </p>
 
           {/* Botão */}
           <Link
-            to="/dashboard/hospital"
-            className="inline-block px-6 py-3 bg-teal-500 text-white font-semibold rounded-lg hover:bg-teal-600 transition cursor-pointer text-sm"
+            to="/dashboard/superadmin"
+            className="inline-block px-6 py-3 bg-cyan-500 text-slate-950 font-semibold rounded-lg hover:bg-cyan-600 transition cursor-pointer text-sm"
           >
             Voltar para o Dashboard
           </Link>
