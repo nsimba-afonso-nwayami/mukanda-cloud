ceac9576195c4bdaaccd9bd23df06f9217cb8781
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-03-25 08:57:49 +0100

    3 componentes adicionados na página home

--- src/pages/site/Home.jsx
+++ src/pages/site/Home.jsx
@@ -1,9 +1,17 @@
 import WhatsAppButton from "../../components/whatsapp/WhatsAppButton";
 
+import Hero from "../../components/home/Hero";
+import QuemSomos from "../../components/home/QuemSomos";
+import ComoFunciona from "../../components/home/ComoFunciona";
+
 export default function Home() {
   return (
     <>
       <title>Mukanda Cloud</title>
+
+      <Hero />
+      <QuemSomos />
+      <ComoFunciona />
 
       {/* Botão WhatsApp fixo */}
       <WhatsAppButton phone="244972614886" size={64} />
