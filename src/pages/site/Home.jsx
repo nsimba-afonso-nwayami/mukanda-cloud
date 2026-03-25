74bb147b05e1a5119970c0ef11689a58166b9fd0
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-03-25 09:48:04 +0100

    Mais 4 componentes adicionados na página home

--- src/pages/site/Home.jsx
+++ src/pages/site/Home.jsx
@@ -3,6 +3,10 @@ import WhatsAppButton from "../../components/whatsapp/WhatsAppButton";
 import Hero from "../../components/home/Hero";
 import QuemSomos from "../../components/home/QuemSomos";
 import ComoFunciona from "../../components/home/ComoFunciona";
+import Precos from "../../components/home/Precos";
+import Faq from "../../components/home/Faq";
+import Contato from "../../components/home/Contato";
+import Cta from "../../components/home/Cta";
 
 export default function Home() {
   return (
@@ -12,6 +16,10 @@ export default function Home() {
       <Hero />
       <QuemSomos />
       <ComoFunciona />
+      <Precos />
+      <Faq />
+      <Contato />
+      <Cta />
 
       {/* Botão WhatsApp fixo */}
       <WhatsAppButton phone="244972614886" size={64} />
